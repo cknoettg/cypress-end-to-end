45d34446042621d195b600bd59b5c3af8ed73c98
cknoettg <cknoettg@yahoo.com>
2020-04-29 22:53:18 -0400

    Added tests

--- cypress/integration/form_test.js
+++ cypress/integration/form_test.js
@@ -1,6 +1,23 @@
 describe("Test our form inputs", function(){
-    Run Test | Debug Test
-    it("visit our localhost", function(){
-    cy.visit("http://localhost:3000");
+    //Run Test | Debug Test
+    this.beforeEach(function() {
+        cy.visit("http://localhost:3000");
+    })
+    it("first test", function(){
+        //cy.get("[for='name'] > input")
+        cy.get('[data-cy="name"]')
+            .type("Corey")
+            .should("have.value", "Corey");
+        cy.get('[data-cy="email"]')
+            .type("email@email.com")
+            .should("have.value", "email@email.com");
+        cy.get("textarea")
+            .type("I want to help")
+            .should("have.value", "I want to help");
+        cy.get("#positions")
+            .select("Yard Work")
+            .should("have.value", "Yard Work");
+        cy.get('[type="checkbox"]').check().should("be.checked");;
+        cy.contains("Submit").click();
     });
 });
